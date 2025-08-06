3804171e25de831761cc8a79750fcda3d40208f0
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2025-08-06 14:19:51 +0100

    Implement entry buttons

--- scripts.js
+++ scripts.js
@@ -28,6 +28,60 @@ function operate(operator, a, b) {
     }
 }
 
+function handleEntry(entry) {
+    if (Number(displayContent) === 0) {
+        displayContent = "";
+    }
+
+    if (entry === "clear") {
+        displayContent = "";
+    } else if (entry === "backspace") {
+        displayContent = displayContent.slice(0, -1);
+    } else if (entry === "decimal") {
+        entry = "."
+    }
+
+    if (isValidEntry(entry)) {
+        displayContent += entry;
+    }
+
+    refreshDisplay();
+}
+
+
+function isValidEntry(entry) {
+    const validEntryRegex = /[\d\.]/
+    if (entry === "." && displayContent.includes(".")){
+        return false;
+    }
+    if (!validEntryRegex.test(entry)) {
+        return false;
+    }
+    return true;
+}
+
+function handleOperation(operator) {
+
+}
+
+function refreshDisplay() {
+    if (!displayContent) {
+        displayContent = "00.00"
+    }
+    document.querySelector("#display").textContent = displayContent;
+}
+
+let displayContent;
 let operator;
 let numA;
 let numB;
+
+document.querySelectorAll("button.entry").forEach(btn => {
+    btn.addEventListener("click", (e) => {handleEntry(e.target.id)});
+});
+
+document.querySelectorAll("button.operator").forEach(btn => {
+    btn.addEventListener("click", (e) => {handleOperation(e.target.id)});
+});
+
+refreshDisplay();
